ac3f02a7ad54973689e80db23e0029bbfb09d007
Y_Less <git@y-less.com>
2018-11-12 01:41:49 +0100

    Missed half the definitions.

--- Internal/NativeFuncImpl.hpp
+++ Internal/NativeFuncImpl.hpp
@@ -17,52 +17,68 @@ namespace pawn_natives
 		cell CallDoInner(AMX * amx, cell * params)
 		{
 			#if PAWN_HOOK_NUMBER >= 1
-				ParamCast<A> a(amx, params, 1);
+				ParamCast<A> a(amx, params, idx);
+				idx += ParamCast<A>::Size;
 			#endif
 			#if PAWN_HOOK_NUMBER >= 2
-				ParamCast<B> b(amx, params, 2);
+				ParamCast<B> b(amx, params, idx);
+				idx += ParamCast<B>::Size;
 			#endif
 			#if PAWN_HOOK_NUMBER >= 3
-				ParamCast<C> c(amx, params, 3);
+				ParamCast<C> c(amx, params, idx);
+				idx += ParamCast<C>::Size;
 			#endif
 			#if PAWN_HOOK_NUMBER >= 4
-				ParamCast<D> d(amx, params, 4);
+				ParamCast<D> d(amx, params, idx);
+				idx += ParamCast<D>::Size;
 			#endif
 			#if PAWN_HOOK_NUMBER >= 5
-				ParamCast<E> e(amx, params, 5);
+				ParamCast<E> e(amx, params, idx);
+				idx += ParamCast<E>::Size;
 			#endif
 			#if PAWN_HOOK_NUMBER >= 6
-				ParamCast<F> f(amx, params, 6);
+				ParamCast<F> f(amx, params, idx);
+				idx += ParamCast<F>::Size;
 			#endif
 			#if PAWN_HOOK_NUMBER >= 7
-				ParamCast<G> g(amx, params, 7);
+				ParamCast<G> g(amx, params, idx);
+				idx += ParamCast<G>::Size;
 			#endif
 			#if PAWN_HOOK_NUMBER >= 8
-				ParamCast<H> h(amx, params, 8);
+				ParamCast<H> h(amx, params, idx);
+				idx += ParamCast<H>::Size;
 			#endif
 			#if PAWN_HOOK_NUMBER >= 9
-				ParamCast<I> i(amx, params, 9);
+				ParamCast<I> i(amx, params, idx);
+				idx += ParamCast<I>::Size;
 			#endif
 			#if PAWN_HOOK_NUMBER >= 10
-				ParamCast<J> j(amx, params, 10);
+				ParamCast<J> j(amx, params, idx);
+				idx += ParamCast<J>::Size;
 			#endif
 			#if PAWN_HOOK_NUMBER >= 11
-				ParamCast<K> k(amx, params, 11);
+				ParamCast<K> k(amx, params, idx);
+				idx += ParamCast<K>::Size;
 			#endif
 			#if PAWN_HOOK_NUMBER >= 12
-				ParamCast<L> l(amx, params, 12);
+				ParamCast<L> l(amx, params, idx);
+				idx += ParamCast<L>::Size;
 			#endif
 			#if PAWN_HOOK_NUMBER >= 13
-				ParamCast<M> m(amx, params, 13);
+				ParamCast<M> m(amx, params, idx);
+				idx += ParamCast<M>::Size;
 			#endif
 			#if PAWN_HOOK_NUMBER >= 14
-				ParamCast<N> n(amx, params, 14);
+				ParamCast<N> n(amx, params, idx);
+				idx += ParamCast<N>::Size;
 			#endif
 			#if PAWN_HOOK_NUMBER >= 15
-				ParamCast<O> o(amx, params, 15);
+				ParamCast<O> o(amx, params, idx);
+				idx += ParamCast<O>::Size;
 			#endif
 			#if PAWN_HOOK_NUMBER >= 16
-				ParamCast<P> p(amx, params, 16);
+				ParamCast<P> p(amx, params, idx);
+				idx += ParamCast<P>::Size;
 			#endif
 				RET
 					ret = this->Do(PAWN_HOOK_CALLING);
